2b9e9abf5568ec7b5f7656c201934c6599c36400
sidharth-14 <sidharthtembhare1244@gmail.com>
2023-08-31 19:36:01 +0530

    initial commit

---
+++ constant.js
@@ -0,0 +1,11 @@
+export const missing_title = '${\\color{red} Title in prompt is missing. Please add and try again.}$';
+
+export const title_length_error = '${\\color{red} Title should have less than 5 words.}$';
+
+export const title_titlecase = '${\\color{red} Title should be in Title Case.}$';
+
+export const missing_labels = '${\\color{red} Labels in prompt is missing.Please add and try again.}$';
+
+export const check_labels = '${\\color{red} Labels in prompt are not correct. Please check labels and try again.}$';
+
+export const missing_body ='${\\color{red} Body in prompt is missing. Please add body try again.}$';
---
+++ index.js
@@ -0,0 +1,96 @@
+import * as core from '@actions/core';
+import axios from 'axios';
+import * as github from '@actions/github';
+import { missing_title, title_length_error, title_titlecase, missing_labels, check_labels, missing_body } from './constant.js';
+
+const PAT = core.getInput('PAT');
+const discussionBody = github.context.payload.discussion.body;
+const lines = discussionBody.split('\n');
+const repoOwner = 'rainfall-one';
+const repoName = 'gitops-test';
+
+let title = '';
+let labels = [];
+let body = '';
+
+const missingFields = [];
+
+for (let i = 1; i < lines.length; i++) {
+
+    const line = lines[i].trim();
+
+    if (line.startsWith('title:')) {
+
+        title = line.replace('title:', '').trim().replaceAll('"', '');
+
+    } else if (line.startsWith('labels:')) {
+        const labelsText = line.replace('labels:', '').trim();
+        labels = JSON.parse(labelsText);
+    } else if (line.startsWith('body:')) {
+
+        body = line.replace('body:', '').trim().replaceAll('"', '');
+
+    }
+}
+if (!title) {
+    missingFields.push(missing_title);
+} else {
+    const titleWords = title.split(' ');
+    if (titleWords.length >= 5) {
+        missingFields.push(title_length_error);
+    } else if (!isTitleCase(title)) {
+        missingFields.push(title_titlecase)
+    }
+}
+if (!labels.length) {
+    missingFields.push(missing_labels);
+}
+
+const headers = {
+    Accept: 'application/vnd.github.v3+json',
+    Authorization: `Bearer ${PAT}`
+};
+
+const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/labels`;
+axios
+    .get(apiUrl, { headers })
+    .then(response => {
+        const repo_labels = response.data.map(label => ({
+            name: label.name,
+            node_id: label.node_id // Retrieve the label ID
+        }));
+        console.log('Labels:', repo_labels);
+
+        if (Array.isArray(infolabel)) {
+            const labelIds = labels.map(labelName => {
+                const label = repo_labels.find(labelObj => labelObj.name === labelName);
+                return label ? label.node_id : null;
+            });
+
+            console.log('Label IDs:', labelIds);
+            core.setOutput("label_id", labelIds);
+        } else {
+            console.error('disc_labels is not an array');
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching labels:', error.message);
+        missingFields.push(check_labels);
+    });
+
+if (!body) {
+    missingFields.push(missing_body);
+}
+
+core.setOutput('if_missing', missingFields.length)
+if (missingFields.length > 0) {
+    const missingFieldsMessage = missingFields.join('\n');
+    const words = missingFieldsMessage.split(' ');
+    const missingFieldsMessages = words.join('\\space ');
+    core.setOutput('Missing', missingFieldsMessages);
+    console.log('Missing Fields:\n', missingFieldsMessages);
+}
+
+core.setOutput('override_title', title);
+core.setOutput('override_labels', labels);
+core.setOutput('override_body', body);
